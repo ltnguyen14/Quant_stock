611475b3ed7501fae28098be3ff2d7ce8368d3fd
Lam Nguyen <ltnguyen14@earlham.edu>
2017-10-01 23:57:11 -0400

    Feedforward

---
+++ feedforward_nn.py
@@ -0,0 +1,97 @@
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from pprint import pprint
+from googlefinance.client import get_price_data, get_prices_data, get_prices_time_data
+import datetime
+
+n_nodes_hl1 = 500
+n_nodes_hl2 = 500
+n_nodes_hl3 = 500
+
+n_classes = 10
+batch_size = 100
+
+x = tf.placeholder('float')
+y = tf.placeholder('float')
+
+def create_data():
+    url = "https://www.quandl.com/api/v3/datasets/CHRIS/CME_CL1.csv"
+    crude_oil = pd.read_csv(url, index_col=0, parse_dates=True)
+    crude_oil.sort_index(inplace=True)
+    crude_oil_last = crude_oil['Last']
+
+    param = {
+            'q': 'XOM',
+            'i': 86400,
+            'x': "NYSEMKT",
+            'p': '40Y'
+    }
+    df = get_price_data(param)
+    df.set_index(df.index.normalize(), inplace=True)
+    stock_close = df['Close']
+
+    oil_price, stock_price = crude_oil_last.align(stock_close, join='inner')
+    return oil_price, stock_price
+
+def neural_network_model(data):
+    hidden_1_layer = {'weights':tf.Variable(tf.random_normal([784, n_nodes_hl1])),
+                      'biases':tf.Variable(tf.random_normal([n_nodes_hl1]))}
+
+    hidden_2_layer = {'weights':tf.Variable(tf.random_normal([n_nodes_hl1, n_nodes_hl2])),
+                      'biases':tf.Variable(tf.random_normal([n_nodes_hl2]))}
+
+    hidden_3_layer = {'weights':tf.Variable(tf.random_normal([n_nodes_hl2, n_nodes_hl3])),
+                      'biases':tf.Variable(tf.random_normal([n_nodes_hl3]))}
+
+    output_layer = {'weights':tf.Variable(tf.random_normal([n_nodes_hl3, n_classes])),
+                    'biases':tf.Variable(tf.random_normal([n_classes])),}
+
+
+    l1 = tf.add(tf.matmul(data,hidden_1_layer['weights']), hidden_1_layer['biases'])
+    l1 = tf.nn.relu(l1)
+
+    l2 = tf.add(tf.matmul(l1,hidden_2_layer['weights']), hidden_2_layer['biases'])
+    l2 = tf.nn.relu(l2)
+
+    l3 = tf.add(tf.matmul(l2,hidden_3_layer['weights']), hidden_3_layer['biases'])
+    l3 = tf.nn.relu(l3)
+
+    output = tf.matmul(l3,output_layer['weights']) + output_layer['biases']
+
+    return output
+
+def train_neural_network(x):
+    prediction = neural_network_model(x)
+    # OLD VERSION:
+    #cost = tf.reduce_mean( tf.nn.softmax_cross_entropy_with_logits(prediction,y) )
+    # NEW:
+    cost = tf.reduce_mean( tf.nn.softmax_cross_entropy_with_logits(logits=prediction, labels=y) )
+    optimizer = tf.train.AdamOptimizer().minimize(cost)
+    oil_price, stock_price = create_data()
+
+    hm_epochs = 10
+    with tf.Session() as sess:
+        # OLD:
+        #sess.run(tf.initialize_all_variables())
+        # NEW:
+        sess.run(tf.global_variables_initializer())
+
+        for epoch in range(hm_epochs):
+            epoch_loss = 0
+            for _ in range(int(len(oil_price)/batch_size)):
+                epoch_x = oil_price[_*batch_size:(_+1)*batch_size]
+                epoch_y = stock_price[_*batch_size:(_+1)*batch_size]
+                _, c = sess.run([optimizer, cost], feed_dict={x: epoch_x, y: epoch_y})
+                epoch_loss += c
+
+            print('Epoch', epoch, 'completed out of',hm_epochs,'loss:',epoch_loss)
+
+        correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
+
+        accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
+        #print('Accuracy:',accuracy.eval({x:mnist.test.images, y:mnist.test.labels}))
+
+train_neural_network(x)
